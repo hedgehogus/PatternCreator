027a3dfe4eea6a17050e8b3bafa53d47356eed13
hedgehogus <research162@gmail.com>
2017-10-28 02:43:15 +0300

    painting in pattern

--- pattern-creator/src/app/pattern.component.ts
+++ pattern-creator/src/app/pattern.component.ts
@@ -6,11 +6,16 @@ import { Pattern } from './pattern';
 @Component({
   selector: 'pattern-component',
   template: `<h1>Pattern</h1>
-            <ul class="pattern">
+            <ul class="pattern" (mouseup)="mUp()"  (mouseleave)="mUp()" >
                 <li class="row" *ngFor = "let row of pattern.cells,let i=index">
                     <ul>
                         <li *ngFor = "let cell of row, let j=index" >   
-                            <div (click)="change(cell,i,j )" class="cell" [class.default]="cell === 0" [class.selected]="cell === 1">  </div>           
+                            <div   
+                                (mousedown)="mDown($event, cell, i, j)"                                                            
+                                (mousemove)="change(i,j , $event)" 
+                                class="cell" 
+                                [class.default]="cell === 0" 
+                                [class.selected]="cell === 1">  </div>           
                         </li>
                     </ul>
                 </li>
@@ -20,7 +25,11 @@ import { Pattern } from './pattern';
 })
 
 export class PatternComponent {
-    pattern: Pattern;    
+    pattern: Pattern;
+    mouseState = false;
+    cellColor:number;
+    lastChangedIndex = {i:-1,
+                        j:-1};
 
     getInitialPattern():void{        
         this.pattern = PATTERN;        
@@ -30,16 +39,31 @@ export class PatternComponent {
         this.getInitialPattern();
     }
 
-    change(cell, i, j){ 
-        //debugger;
-            
-        if (cell === 0){
-            this.pattern.cells[i][j] = 1;
-        } else if (cell === 1){
-            this.pattern.cells[i][j] = 0;
-        }        
-        //console.log(cell, i,j);
-        
+    change(i, j , event){
+        if (this.mouseState ){        
+            if (!(i === this.lastChangedIndex.i && j === this.lastChangedIndex.j)){
+                this.pattern.cells[i][j] = this.cellColor;                
+                this.lastChangedIndex = {i,j};  
+            }
+        }
     }
+
+    mDown(e, cell, i, j){
+        e.preventDefault();
+        this.mouseState = true;
+        if (cell === 0){
+            this.cellColor = 1;
+        } else if (cell === 1){
+            this.cellColor = 0;
+        };    
+        this.change(i,j,e);
+        console.log("down");
+    };
+
+    mUp(){
+        this.mouseState = false;        
+        this.lastChangedIndex = {i:-1,j:-1};
+        console.log("up");
+    };
     
 }
