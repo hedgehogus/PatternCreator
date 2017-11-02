e890530bb77eb49f19e68de8e0a623a26f820ee2
hedgehogus <research162@gmail.com>
2017-11-02 21:21:06 +0200

    picker

--- pattern-creator/src/app/color.component.ts
+++ pattern-creator/src/app/color.component.ts
@@ -3,7 +3,14 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'color-component',
-  template: `<h1>color component</h1>` ,
+  template: `<h1>color component</h1>  
+            <div class="displayColor"> </div>
+            <div class="picker">       
+                <input type="range" name="r" min="0" max ="254">
+                <input type="range" name="g" min="0" max ="254">
+                <input type="range" name="b" min="0" max ="254">
+            </div>` 
+            ,
   styleUrls: ['./color.component.css']
 })
 
