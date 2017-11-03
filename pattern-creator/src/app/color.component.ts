4fce563dca95f63a256e53de2df5494f22169750
hedgehogus <research162@gmail.com>
2017-11-03 03:53:16 +0200

    hexadecimal color

--- pattern-creator/src/app/color.component.ts
+++ pattern-creator/src/app/color.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { Color } from './color'
 
 
 @Component({
   selector: 'color-component',
   template: `<h1>color component</h1>  
             <div class="displayColor"> </div>
+            <p>{{getCurrent()}}</p>
             <div class="picker">       
                 <input type="range" name="r" min="0" max ="254">
                 <input type="range" name="g" min="0" max ="254">
@@ -15,4 +17,13 @@ import { Component } from '@angular/core';
 })
 
 export class ColorComponent {
+    color: Color;
+    
+    ngOnInit(): void {        
+        this.color = {r:100,g:66,b:77};
+    }
+
+    getCurrent(){
+        return '#' + this.color.r.toString(16) + this.color.g.toString(16) + this.color.b.toString(16);
+    }
 }
