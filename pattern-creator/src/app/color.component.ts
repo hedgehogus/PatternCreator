c10af653a7ee6bd8ff125e69ddcd2917bb7706cf
hedgehogus <research162@gmail.com>
2017-11-05 03:23:33 +0200

    back ang main color changing

--- pattern-creator/src/app/color.component.ts
+++ pattern-creator/src/app/color.component.ts
@@ -11,34 +11,50 @@ import { Color } from './color';
                 <input type="radio" id="main" name="mainorback" [value]="0" [(ngModel)]="radio.value">  
                 <label for="main">main color<label>              
                 <input type="radio" id="back" name="mainorback" [value]="1" [(ngModel)]="radio.value">
-                <label for="back">background color<label>   
-                <p>{{radio.value}}</p>
+                <label for="back">background color<label>                
             <div> 
             <div class="displayColor" [ngStyle]="changeBackground()" > </div>            
-            <div class="picker">       
+            <div class="mainpicker" *ngIf="radio.value === 0">                  
                 <input type="range" name="r" min="0" max ="254"
-                    [(ngModel)]="color.r">
+                    [(ngModel)]="backColor.r">
                 <input type="range" name="g" min="0" max ="254"
-                    [(ngModel)]="color.g">
+                    [(ngModel)]="backColor.g">
                 <input type="range" name="b" min="0" max ="254"
-                    [(ngModel)]="color.b">
+                    [(ngModel)]="backColor.b">
+            </div>
+            <div class="backpicker" *ngIf="radio.value === 1">       
+                <input type="range" name="r" min="0" max ="254"
+                    [(ngModel)]="mainColor.r">
+                <input type="range" name="g" min="0" max ="254"
+                    [(ngModel)]="mainColor.g">
+                <input type="range" name="b" min="0" max ="254"
+                    [(ngModel)]="mainColor.b">
             </div>` 
             ,
   styleUrls: ['./color.component.css']
 })
 
 export class ColorComponent {
-    color: Color;
+    backColor: Color;
+    mainColor: Color;
     radio = {value:0};
     
     
     ngOnInit(): void {        
-        this.color = {r:100,g:66,b:77};        
+        this.backColor = {r:100,g:66,b:77};   
+        this.mainColor = {r:250,g:66,b:77};     
     }
 
     getCurrent(){
-        console.log(this.radio.value);
-        return '#' + this.color.r.toString(16) + this.color.g.toString(16) + this.color.b.toString(16);
+        let color;
+        switch (this.radio.value){
+            case 0:
+                color = this.backColor;
+                break;
+            case 1:
+                color = this.mainColor;
+        }        
+        return '#' + color.r.toString(16) + color.g.toString(16) + color.b.toString(16);
     }
 
     changeBackground(): any {
